540c76075c693cc175304e7fdb0df70a550eaa68
22Mouni2005 <mounikaf6@gmail.com>
2025-07-17 21:49:42 +0530

    Time: 4 ms (17.97%), Space: 17.8 MB (51.85%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,12 +1,28 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        s=0
-        for i in digits:
-            s=s*10+i
-        s=s+1
-        l=[]
-        while s>0:
-            l.insert(0,s%10)
-            s=s//10
-        return l
-        
+    def addBinary(self, a: str, b: str) -> str:
+        asum=0
+        s=""
+        bs=0
+        j=0
+        for i in range(len(a)-1,-1,-1):
+            asum+=(int(a[i])*(2**j))
+            j+=1
+        j=0
+        for i in range(len(b)-1,-1,-1):
+            bs+=(int(b[i])*(2**j))
+            j+=1
+        res=asum+bs
+        if res==0:
+            return "0"
+        while res>=2:
+            if res==1:
+                s+="1"
+            elif res==0:
+                s+="0"
+            else:
+                r=res%2
+                s+=str(r)
+                res=res//2
+        if res==1:
+            s+="1"
+        return s[::-1]
