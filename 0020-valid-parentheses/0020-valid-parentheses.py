6f5187c5b18e63225e54955a9bfb76c1ae38cca2
Karri Mounika <mounikaf6@gmail.com>
2025-07-25 20:53:59 +0530

    Time: 0 ms (100%), Space: 18 MB (9.88%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)%2!=0:
+            return False
+        stack,opens,closes=[],['(','[','{'],[')',']','}']
+        for i in s:
+            if i in opens:
+                stack.append(i)
+            if i in closes:
+                if stack and closes.index(i)==opens.index(stack[-1]):
+                    stack.pop()
+                else:
+                    return False
+        if len(stack)==0:
+            return True
+        return False
+        
+
