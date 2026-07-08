e5073704eeb06713ff1361f13813d42e240ae607
Karri Mounika <mounikaf6@gmail.com>
2026-07-08 14:30:07 +0530

    Time: 7 ms (37.4%), Space: 19.2 MB (46.09%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,13 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        d={}
+        mapst,mapts={},{}
         for i in range(len(s)):
-            if s[i] not in d :
-                if t[i] in d.values():
-                    return False
-                d[s[i]]=t[i]
-            else:
-                if d[s[i]] !=t[i]:
-                    return False
+            chars,chart=s[i],t[i]
+            if chars in mapst and mapst[chars]!=chart:
+                return False
+            if chart in mapts and mapts[chart]!=chars:
+                return False
+            mapst[chars]=chart
+            mapts[chart]=chars
         return True
         
