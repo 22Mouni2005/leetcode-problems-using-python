0c1e1bd1e046b29b09a5aaebf6d53b6e62925ab5
Karri Mounika <mounikaf6@gmail.com>
2026-07-15 10:00:33 +0530

    Time: 28 ms (18.07%), Space: 19.5 MB (93.73%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d={}
+        for i in range(len(magazine)):
+            d[magazine[i]]=d.get(magazine[i],0)+1
+        for i in ransomNote:
+            if d.get(i,0)==0:
+                return False
+            d[i]-=1
+        return True
+
+
+        
